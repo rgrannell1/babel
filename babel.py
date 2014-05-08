894d8f08a4b9de5066a15cbfbae6255036e93a3e
Ryan Grannell <r.grannell2@gmail.com>
2014-05-09 00:14:37 +0100

    app now has basic functionality.

--- babel.py
+++ babel.py
@@ -1,3 +1,4 @@
+
 import sublime, sublime_plugin, os, random
 import itertools
 from random import sample
@@ -16,22 +17,49 @@ def secondOf (coll):
 def thirdOf (coll):
 	return coll[2]
 
+
+
+
+
+
+
+
+
+
+
+# -- other stuff
+def get_random_file (folder):
+	# -- string -> string
+	# -- return a random file somewhere recursively within a folder.
+
+	contents = list(os.walk(folder))[0]
+
+	files = [firstOf(contents) + '/' + f for f in contents[2]]
+
+	return(firstOf(sample(files, 1)))
+
+def open_file (abspath):
+	# string -> None
+	# opens an absolute path.
+
+	self.window.open_file(chosen_file, sublime.TRANSIENT)
+
+
+
+
+
+
+
+
+
+
 class BabelCommand( sublime_plugin.WindowCommand ):
-	def run( self ):
+	def run (self):
 
 		open_folders = self.window.folders()
-
-		def get_random_file (folder):
-			# -- string -> string
-			# -- return a random file somewhere recursively within a folder.
-
-			contents = list(os.walk(folder))[0]
-
-			files = [firstOf(contents) + '/' + f for f in contents[2]]
-
-			return(firstOf(sample(files, 1)))
 
 		chosen_files = [get_random_file(folder) for folder in open_folders]
 		chosen_file  = firstOf(sample(chosen_files, 1))
 
-		self.window.open_file(chosen_file, sublime.TRANSIENT)
+		open_file(chosen_file)
+
